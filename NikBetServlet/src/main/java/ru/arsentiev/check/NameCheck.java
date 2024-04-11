6abc02a84414d7761fdc79a8d79fc1cf7b095fb3
srBob01 <necit717@gmail.com>
2024-04-11 23:21:44 +0300

    add check for lastname and firstname fields

---
+++ NikBetServlet/src/main/java/ru/arsentiev/check/NameCheck.java
@@ -0,0 +1,23 @@
+package ru.arsentiev.check;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+
+public class NameCheck {
+    private static final NameCheck INSTANCE = new NameCheck();
+
+    public static NameCheck getInstance() {
+        return INSTANCE;
+    }
+
+    private final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z]{1,40}");
+
+    public boolean check(String string) {
+        return NAME_PATTERN.matcher(string).matches();
+    }
+
+}
