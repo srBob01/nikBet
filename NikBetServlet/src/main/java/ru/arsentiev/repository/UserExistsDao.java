33edacbf8c96b46caa126188b98694612a9a4b87
srBob01 <necit717@gmail.com>
2024-04-11 23:20:19 +0300

    add dao for check unique

---
+++ NikBetServlet/src/main/java/ru/arsentiev/repository/UserExistsDao.java
@@ -0,0 +1,48 @@
+package ru.arsentiev.repository;
+
+import ru.arsentiev.connection.ConnectionManager;
+import ru.arsentiev.exception.DaoException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserExistsDao {
+    private final ConnectionManager connectionManager;
+
+    public UserExistsDao(ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    //language=PostgreSQL
+    private static final String SELECT_1_BY_EMAIL = "SELECT 1 FROM users WHERE email = ?;";
+    //language=PostgreSQL
+    private static final String SELECT_1_BY_NICKNAME = "SELECT 1 FROM users WHERE nickname = ?;";
+    //language=PostgreSQL
+    private static final String SELECT_1_BY_PHONE_NUMBER = "SELECT 1 FROM users WHERE phonenumber = ?;";
+
+    public boolean existsByEmail(String email) {
+        return executeMyQuery(email, SELECT_1_BY_EMAIL);
+    }
+
+    public boolean existsByNickname(String nickname) {
+        return executeMyQuery(nickname, SELECT_1_BY_NICKNAME);
+    }
+
+    public boolean existsByPhoneNumber(String phoneNumber) {
+        return executeMyQuery(phoneNumber, SELECT_1_BY_PHONE_NUMBER);
+    }
+
+    private boolean executeMyQuery(String nickname, String select1ByNickname) {
+        try (Connection connection = connectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(select1ByNickname)) {
+            preparedStatement.setString(1, nickname);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException | InterruptedException e) {
+            throw new DaoException(e);
+        }
+    }
+}
