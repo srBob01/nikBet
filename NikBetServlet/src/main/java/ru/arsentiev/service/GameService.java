4f5e7d16efd031ba20764e026fc2f891c26014fb
srBob01 <necit717@gmail.com>
2024-04-18 17:21:55 +0300

    add GameService

---
+++ NikBetServlet/src/main/java/ru/arsentiev/service/GameService.java
@@ -0,0 +1,44 @@
+package ru.arsentiev.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import ru.arsentiev.dto.game.GameViewCompletedDto;
+import ru.arsentiev.dto.game.GameViewInProgressDto;
+import ru.arsentiev.dto.game.GameViewScheduledDto;
+import ru.arsentiev.manager.DaoManager;
+import ru.arsentiev.mapper.GameMapper;
+import ru.arsentiev.repository.GameDao;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GameService {
+    private static final GameService INSTANCE = new GameService();
+    private static final GameDao gameDao = DaoManager.getGameDao();
+    private static final GameMapper gameMapper = GameMapper.getInstance();
+
+    public static GameService getInstance() {
+        return INSTANCE;
+    }
+
+    public List<GameViewInProgressDto> selectGameInProgress() {
+        return gameDao.selectAllGameInProgress().stream()
+                .map(gameMapper::mapViewInProgress)
+                .collect(toList());
+    }
+
+    public List<GameViewScheduledDto> selectGameScheduled() {
+        return gameDao.selectAllGameScheduled().stream()
+                .map(gameMapper::mapViewScheduled)
+                .collect(toList());
+    }
+
+    public List<GameViewCompletedDto> selectGameCompleted() {
+        return gameDao.selectAllGameCompleted().stream()
+                .map(gameMapper::mapViewCompleted)
+                .collect(toList());
+    }
+}
