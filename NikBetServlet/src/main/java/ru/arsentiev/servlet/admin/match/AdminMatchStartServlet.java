9ced746501cd837a6fb4a1671204486cb138ec9f
srBob01 <necit717@gmail.com>
2024-04-22 05:31:38 +0300

    add admin start game page

---
+++ NikBetServlet/src/main/java/ru/arsentiev/servlet/admin/match/AdminMatchStartServlet.java
@@ -0,0 +1,50 @@
+package ru.arsentiev.servlet.admin.match;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.arsentiev.dto.game.view.GameScheduledViewDto;
+import ru.arsentiev.mapper.GameMapper;
+import ru.arsentiev.service.GameService;
+import ru.arsentiev.utils.JspPathCreator;
+
+import java.io.IOException;
+import java.util.List;
+
+import static ru.arsentiev.utils.JspPathGetter.ADMIN_MATCH_START_JSP;
+import static ru.arsentiev.utils.UrlPathGetter.ADMIN_MATCH_START_URL;
+
+@WebServlet(ADMIN_MATCH_START_URL)
+public class AdminMatchStartServlet extends HttpServlet {
+    private GameService gameService;
+    private GameMapper gameMapper;
+
+    @Override
+    public void init() throws ServletException {
+        gameMapper = GameMapper.getInstance();
+        gameService = GameService.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<GameScheduledViewDto> listGameScheduledDto = gameService.selectHotGame()
+                .stream()
+                .map(gameMapper::mapScheduledControllerToView)
+                .toList();
+        req.setAttribute("listGameScheduledDto", listGameScheduledDto);
+        req.getRequestDispatcher(JspPathCreator.getAdminPath(ADMIN_MATCH_START_JSP)).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long idGame = Long.parseLong(req.getParameter("idGame"));
+        if (gameService.startGame(idGame)) {
+            req.setAttribute("messageGood", "You started the game with an id " + idGame);
+        } else {
+            req.setAttribute("messageBad", "The game with the id " + idGame + " has not been started");
+        }
+        doGet(req, resp);
+    }
+}
