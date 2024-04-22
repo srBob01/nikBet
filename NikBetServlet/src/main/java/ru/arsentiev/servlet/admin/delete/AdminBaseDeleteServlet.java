ce3ebb2104a722828a4fc57f0cf7d863e5f4643f
srBob01 <necit717@gmail.com>
2024-04-22 05:30:26 +0300

    add AdminBaseDeleteServlet

---
+++ NikBetServlet/src/main/java/ru/arsentiev/servlet/admin/delete/AdminBaseDeleteServlet.java
@@ -0,0 +1,33 @@
+package ru.arsentiev.servlet.admin.delete;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.arsentiev.mapper.UserMapper;
+import ru.arsentiev.service.UserService;
+
+import java.io.IOException;
+
+public class AdminBaseDeleteServlet extends HttpServlet {
+    protected UserService userService;
+
+    protected UserMapper userMapper;
+
+    @Override
+    public void init() throws ServletException {
+        userService = UserService.getInstance();
+        userMapper = UserMapper.getInstance();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idUser = req.getParameter("userId");
+        if (userService.deleteUser(Long.parseLong(idUser))) {
+            req.setAttribute("message", "The user with the id " + idUser + " has been successfully deleted");
+        } else {
+            req.setAttribute("message", "error. The user with the id " + idUser + " has not been deleted");
+        }
+        doGet(req, resp);
+    }
+}
